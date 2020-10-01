c811a4a8eb8cf86a120df9f2bd4db09752479bd8
Edgar Negron <edgar.negron.rabell@gmail.com>
2020-10-01 16:30:05 -0400

    Initialize repository and create basic Header and add Firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2I_W3hKRis2UmFcJFDKf-zAFy3L0A2pg",
+  authDomain: "facebook-clone-3aa70.firebaseapp.com",
+  databaseURL: "https://facebook-clone-3aa70.firebaseio.com",
+  projectId: "facebook-clone-3aa70",
+  storageBucket: "facebook-clone-3aa70.appspot.com",
+  messagingSenderId: "1055878949466",
+  appId: "1:1055878949466:web:4146c3be6c2bca396e8663",
+  measurementId: "G-V39CHQSSWZ"
+};
+
